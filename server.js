9fa025a54c988cf81201f19f20e43606d6678e17
Shaya Zarkesh <shaya@polyup.com>
2022-01-14 22:47:52 -0500

    nice

--- server.js
+++ server.js
@@ -1,37 +1,49 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const client = require("twilio")("AC137737e38e5f757d10fa5c5054465a70", "d4dc47939b44a2a7b0a91234bf6665d2");
-const path = require("path");
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const client = require("twilio")("ACd5b8214ab5a4060171d3e5a21b1d8012", "ffed8b111c7612bb51295025e34f7569")
+const path = require("path")
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors({ origin: true, credentials: true }))
+app.use(bodyParser.json())
 
-const apiPort = process.env.PORT || 3001;
+const apiPort = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")))
 
 app.post("/api/messages", (req, res) => {
-    res.header("Content-Type", "application/json");
-    console.log(req.body);
-    client.messages
-        .create({
-            from: "2055512859",
-            to: req.body.to,
-            body: req.body.body,
-        })
+    res.header("Content-Type", "application/json")
+    console.log(req.body)
+    Promise.all([
+        client.messages.create({
+            from: "9034943977",
+            to: req.body.phone1,
+            body: req.body.message1,
+        }),
+        client.messages.create({
+            from: "9034943977",
+            to: req.body.phone2,
+            body: req.body.message2,
+        }),
+    ])
         .then(() => {
-            console.log("SENT MESSAGE FROM " + req.body.body);
-            res.send(JSON.stringify({ success: true }));
+            console.log("SENT MESSAGE FROM " + req.body.body)
+            res.send(JSON.stringify({ success: true }))
         })
         .catch((err) => {
-            console.log(err);
-            res.send(JSON.stringify({ success: false }));
-        });
-});
+            console.log(err)
+            res.send(JSON.stringify({ success: false }))
+        })
+})
+
+app.get("/test", (req, res) => {
+    res.send("hello, world")
+})
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/build/index.html"));
-});
+    res.send("oops")
+})
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
